c7ce50a878c38450ea00a41902abfaaa465b1aaa
AliceLin321 <ataechiml24@gmail.com>
2022-11-30 13:56:55 -0500

    Alice Lin Vite

---
+++ vite-project/js/main.js
@@ -0,0 +1,7 @@
+import "../style/style.css";
+import javascriptLogo from "./javascript.svg";
+import { setupCounter } from "./counter.js";
+
+document.querySelector("#app").innerHTML = `
+<h1>Hello</h1>
+
