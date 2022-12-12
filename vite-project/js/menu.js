a33896ae58eb06a07b43d6472b74714290c6107c
AliceLin321 <ataechiml24@gmail.com>
2022-12-12 13:56:14 -0500

    Alice Lin Bon VIte App

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,12 +1,81 @@
 const menu = [
-    {name: "Beef Pho",
-    vegan: "",
-    price:3,
-    img:"",
-    available: t,
+  {
+    name: "Beef Pho",
+    vegan: false,
+    price: $12,
+    img: "https://www.sainsburysmagazine.co.uk/media/5222/download/Vietnamese-beef-pho-1120.jpg?v=1",
+    halal: true,
+  },
+  {
+    name: "Veggie Pho",
+    vegan: true,
+    price: $11,
+    img: "https://www.simplysogood.com/wp-content/uploads/2018/01/untitled-8-1.jpg",
+    halal: true,
+  },
+  {
+    name: "Spring Roll",
+    vegan: false,
+    price: $6,
+    img: "https://cookieandkate.com/images/2019/08/fresh-spring-rolls-recipe-4.jpg",
+    halal: true,
+  },
 
-    },
+  {
+    name: "Lumpia",
+    vegan: false,
+    price: $6,
+    img: "http://3.bp.blogspot.com/-YKdmCvLyHdQ/VBSNbDMlBPI/AAAAAAAAbRE/XbxTlO2xQ4g/s1600/vietnamesespringrolls.jpg",
+    halal: false,
+  },
+  {
+    name: "ham bahn mi",
+    vegan: false,
+    price: $7,
+    img: "https://www.happyfoodstube.com/wp-content/uploads/2018/08/vietnamese-sandwich-banh-mi-image.jpg",
+    halal: false,
+  },
 
- 
+  {
+    name: "grilled pork bahn mi",
+    vegan: false,
+    price: $7,
+    img: "https://rasamalaysia.com/wp-content/uploads/2012/06/lemongrass-pork-banhmi-thum-1.jpg",
+    halal: false,
+  },
+
+  {
+    name: "curry chicken w/ bread",
+    vegan: false,
+    price: $10,
+    img: "https://www.wandercooks.com/wp-content/uploads/2022/01/vietnamese-chicken-curry-5-683x1024.jpg",
+    halal: t,
+  },
+
+  { name: "", vegan: f, price: 3, img: "", halal: t },
+
+  { name: "", vegan: f, price: 3, img: "", halal: t },
+
+  { name: "", vegan: f, price: 3, img: "", halal: t },
+
+  { name: "", vegan: f, price: 3, img: "", halal: t },
+
+  { name: "", vegan: f, price: 3, img: "", halal: t },
+
+  { name: "", vegan: f, price: 3, img: "", halal: t },
+
+  { name: "", vegan: f, price: 3, img: "", halal: t },
+
+  { name: "", vegan: f, price: 3, img: "", halal: t },
+
+  { name: "", vegan: f, price: 3, img: "", halal: t },
+
+  { name: "", vegan: f, price: 3, img: "", halal: t },
+
+  { name: "", vegan: f, price: 3, img: "", halal: t },
+
+  { name: "", vegan: f, price: 3, img: "", halal: t },
+
+  { name: "", vegan: f, price: 3, img: "", halal: t },
 ];
-export {menu}
+export { menu };
