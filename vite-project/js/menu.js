ef7dc3761247a8f25c9acd892f8ea81e066f64cb
AliceLin321 <ataechiml24@gmail.com>
2022-12-13 13:51:55 -0500

    Alice Lin Bon Vite

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -49,33 +49,31 @@ const menu = [
     vegan: false,
     price: $10,
     img: "https://www.wandercooks.com/wp-content/uploads/2022/01/vietnamese-chicken-curry-5-683x1024.jpg",
-    halal: t,
+    halal: true,
   },
 
-  { name: "", vegan: f, price: 3, img: "", halal: t },
+  {
+    name: "bun cha",
+    vegan: false,
+    price: $9,
+    img: "https://image.essen-und-trinken.de/11950450/t/Ax/v10/w1440/r1.7778/-/fleisch-nudel-bowl-bun-cha-jpg--64061-.jpg",
+    halal: false,
+  },
 
-  { name: "", vegan: f, price: 3, img: "", halal: t },
+  { name: "bahn xeo crepe", vegan: false, price: $7, img: "https://takestwoeggs.com/wp-content/uploads/2022/04/Ba%CC%81nh-Xe%CC%80o-Vietnamese-Cre%CC%82pes-Takestwoeggs-Final-Photogrphy-sq.jpg", halal: true },
 
-  { name: "", vegan: f, price: 3, img: "", halal: t },
+  { name: "Veggie Bun bo Hue", vegan: true, price: $11, img: "https://woonheng.com/wp-content/uploads/2020/12/Bun-Bo-Hue-Chay-1-scaled.jpg", halal: true },
 
-  { name: "", vegan: f, price: 3, img: "", halal: t },
+  { name: "Cao Lau", vegan: false, price: $8, img: "https://hoianprivatetaxi.com/wp-content/uploads/2021/09/Cao-Lau-Hoi-An-Hoi-An-Private-Taxi-2.jpg", halal: false },
 
-  { name: "", vegan: f, price: 3, img: "", halal: t },
+  { name: "cha ca", vegan: false, price: $9, img: "https://www.cooking-therapy.com/wp-content/uploads/2020/06/Vietnamese-Turmeric-Fish-scaled.jpg", halal: true },
 
-  { name: "", vegan: f, price: 3, img: "", halal: t },
+  { name: "ga nuong", vegan: false, price: $12, img: "https://www.theforkbite.com/wp-content/uploads/2019/11/Bun-Ga-Nuong-featured-1200x1200-1.jpg", halal: true },
 
-  { name: "", vegan: f, price: 3, img: "", halal: t },
+  { name: "Nom hoa chuoi", vegan: true, price: $6, img: "https://www.vietnamonline.com/media/uploads/froala_editor/images/VNO_NomHoaChuoi.jpg", halal: true },
 
-  { name: "", vegan: f, price: 3, img: "", halal: t },
+  { name: "Banh khot", vegan: false, price: 3, img: "", halal: true },
 
-  { name: "", vegan: f, price: 3, img: "", halal: t },
-
-  { name: "", vegan: f, price: 3, img: "", halal: t },
-
-  { name: "", vegan: f, price: 3, img: "", halal: t },
-
-  { name: "", vegan: f, price: 3, img: "", halal: t },
-
-  { name: "", vegan: f, price: 3, img: "", halal: t },
+  { name: "bahn knot", vegan: false, price: 3, img: "https://www.wokandkin.com/wp-content/uploads/2021/02/Banh-Khot-Close-Up-saved-for-web.png", halal: true },
 ];
 export { menu };
