ecb1e05c07f5e0fc37d676ed91b99dab510a30cb
AliceLin321 <ataechiml24@gmail.com>
2022-12-14 13:56:48 -0500

    Alice Lin Bon apep vite

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,4 +1,8 @@
 import "../styles/style.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 import {menu} from "./menu";
 console.log(menu);
 
