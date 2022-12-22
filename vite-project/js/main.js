4efa43fb62e8b3016bbfdbdfd04bb0e75e5eed82
AliceLin321 <ataechiml24@gmail.com>
2022-12-22 13:01:05 -0500

    rsthe5yjhe

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,18 +1,47 @@
 import "../styles/style.css";
-import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
-// ..
-AOS.init();
+
 import { menu } from "./menu.js";
 
-const Dom = {
+const DOM = {
   menupage: document.getElementById("menupage"),
+  btn: document.getElementById("btn"),
 };
 
-menu
-  .filter((menu) => menu.vegan === "true")
-  .forEach((menu) =>
-    Dom.menupage.insertAdjacentHTML("beforeend", `<h1>${menu.name}</h1>`)
-  );
+DOM.btn.addEventListener("click", function () {
+  console.log("hi");
+});
 
-  Dom.menupage.insertAdjacentHTML("beforeend")
+/*menu.forEach((menu) =>
+  DOM.menupage.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="child">
+  <img class="img" src="${menu.img}"><img>
+  <h1 class="price">${menu.name}</h1>
+  <h2 class="price">$${menu.price}</h2>
+  </div>
+  `
+  )
+);*/
+
+DOM.btn.addEventListener("click", function () {
+  DOM.menupage.innerHTML = "";
+  screwvegan();
+});
+
+function screwvegan() {
+  menu
+    .filter((menu) => menu.vegan === "false")
+    .forEach((menu) =>
+      DOM.menupage.insertAdjacentHTML(
+        "beforeend",
+        `
+  <div class="child">
+<img class="img" src="${menu.img}"><img>
+<h1 class="price">${menu.name}</h1>
+<h2 class="price">$${menu.price}</h2>
+</div>
+`
+      )
+    );
+}
--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,119 +1,119 @@
 const menu = [
   {
     name: "Beef Pho",
-    vegan: false,
+    vegan: "false",
     price: 12,
     img: "https://www.sainsburysmagazine.co.uk/media/5222/download/Vietnamese-beef-pho-1120.jpg?v=1",
-    halal: true,
+    halal: "true",
   },
   {
     name: "Veggie Pho",
-    vegan: true,
+    vegan: "true",
     price: 11,
     img: "https://www.simplysogood.com/wp-content/uploads/2018/01/untitled-8-1.jpg",
-    halal: true,
+    halal: "true",
   },
   {
     name: "Spring Roll",
-    vegan: false,
+    vegan: "false",
     price: 6,
     img: "https://cookieandkate.com/images/2019/08/fresh-spring-rolls-recipe-4.jpg",
-    halal: true,
+    halal: "true",
   },
 
   {
     name: "Lumpia",
-    vegan: false,
+    vegan: "false",
     price: 6,
     img: "http://3.bp.blogspot.com/-YKdmCvLyHdQ/VBSNbDMlBPI/AAAAAAAAbRE/XbxTlO2xQ4g/s1600/vietnamesespringrolls.jpg",
-    halal: false,
+    halal: "false",
   },
   {
     name: "ham bahn mi",
-    vegan: false,
+    vegan: "false",
     price: 7,
     img: "https://www.happyfoodstube.com/wp-content/uploads/2018/08/vietnamese-sandwich-banh-mi-image.jpg",
-    halal: false,
+    halal: "false",
   },
 
   {
     name: "grilled pork bahn mi",
-    vegan: false,
+    vegan: "false",
     price: 7,
     img: "https://rasamalaysia.com/wp-content/uploads/2012/06/lemongrass-pork-banhmi-thum-1.jpg",
-    halal: false,
+    halal: "false",
   },
 
   {
     name: "curry chicken w/ bread",
-    vegan: false,
+    vegan: "false",
     price: 10,
     img: "https://www.wandercooks.com/wp-content/uploads/2022/01/vietnamese-chicken-curry-5-683x1024.jpg",
-    halal: true,
+    halal: "true",
   },
 
   {
     name: "bun cha",
-    vegan: false,
+    vegan: "false",
     price: 9,
     img: "https://image.essen-und-trinken.de/11950450/t/Ax/v10/w1440/r1.7778/-/fleisch-nudel-bowl-bun-cha-jpg--64061-.jpg",
-    halal: false,
+    halal: "false",
   },
 
   {
     name: "bahn xeo crepe",
-    vegan: false,
+    vegan: "false",
     price: 7,
     img: "https://takestwoeggs.com/wp-content/uploads/2022/04/Ba%CC%81nh-Xe%CC%80o-Vietnamese-Cre%CC%82pes-Takestwoeggs-Final-Photogrphy-sq.jpg",
-    halal: true,
+    halal: "true",
   },
 
   {
     name: "Veggie Bun bo Hue",
-    vegan: true,
+    vegan: "true",
     price: 11,
     img: "https://woonheng.com/wp-content/uploads/2020/12/Bun-Bo-Hue-Chay-1-scaled.jpg",
-    halal: true,
+    halal: "true",
   },
 
   {
     name: "Cao Lau",
-    vegan: false,
+    vegan: "false",
     price: 8,
     img: "https://hoianprivatetaxi.com/wp-content/uploads/2021/09/Cao-Lau-Hoi-An-Hoi-An-Private-Taxi-2.jpg",
-    halal: false,
+    halal: "false",
   },
 
   {
     name: "cha ca",
-    vegan: false,
+    vegan: "false",
     price: 9,
     img: "https://www.cooking-therapy.com/wp-content/uploads/2020/06/Vietnamese-Turmeric-Fish-scaled.jpg",
-    halal: true,
+    halal: "true",
   },
 
   {
     name: "ga nuong",
-    vegan: false,
+    vegan: "false",
     price: 12,
     img: "https://www.theforkbite.com/wp-content/uploads/2019/11/Bun-Ga-Nuong-featured-1200x1200-1.jpg",
-    halal: true,
+    halal: "true",
   },
 
   {
     name: "Nom hoa chuoi",
-    vegan: true,
+    vegan: "true",
     price: 6,
     img: "https://www.vietnamonline.com/media/uploads/froala_editor/images/VNO_NomHoaChuoi.jpg",
-    halal: true,
+    halal: "true",
   },
 
   {
     name: "bahn knot",
-    vegan: false,
+    vegan: "false",
     price: 3,
     img: "https://www.wokandkin.com/wp-content/uploads/2021/02/Banh-Khot-Close-Up-saved-for-web.png",
-    halal: true,
+    halal: "true",
   },
 ];
 export { menu };
