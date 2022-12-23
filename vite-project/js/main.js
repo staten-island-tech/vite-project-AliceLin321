4b811597211eb979f1617cf2c6f5dc0b853914de
AliceLin321 <ataechiml24@gmail.com>
2022-12-23 13:50:16 -0500

    Alice Lin Bon Vitte App
     .reg

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -5,6 +5,10 @@ import { menu } from "./menu.js";
 const DOM = {
   menupage: document.getElementById("menupage"),
   btn: document.getElementById("btn"),
+  nonvegan: document.getElementById("nonvegan"),
+  halal: document.getElementById("halal"),
+  nonhalal: document.getElementById("nothalal"),
+  mainmenu: document.getElementById("everything"),
 };
 
 DOM.btn.addEventListener("click", function () {
@@ -24,6 +28,39 @@ DOM.btn.addEventListener("click", function () {
   )
 );*/
 
+menu.forEach((menu) =>
+  DOM.menupage.insertAdjacentHTML(
+    "beforeend",
+    `
+  <div class="child">
+<img class="img" src="${menu.img}"><img>
+<h1 class="price">${menu.name}</h1>
+<h2 class="price">$${menu.price}</h2>
+</div>
+`
+  )
+);
+
+DOM.mainmenu.addEventListener("click", function () {
+  DOM.menupage.innerHTML = "";
+  all();
+});
+
+function all() {
+  menu.forEach((menu) =>
+    DOM.menupage.insertAdjacentHTML(
+      "beforeend",
+      `
+  <div class="child">
+<img class="img" src="${menu.img}"><img>
+<h1 class="price">${menu.name}</h1>
+<h2 class="price">$${menu.price}</h2>
+</div>
+`
+    )
+  );
+}
+
 DOM.btn.addEventListener("click", function () {
   DOM.menupage.innerHTML = "";
   screwvegan();
@@ -31,7 +68,71 @@ DOM.btn.addEventListener("click", function () {
 
 function screwvegan() {
   menu
+    .filter((menu) => menu.vegan === "true")
+    .forEach((menu) =>
+      DOM.menupage.insertAdjacentHTML(
+        "beforeend",
+        `
+  <div class="child">
+<img class="img" src="${menu.img}"><img>
+<h1 class="price">${menu.name}</h1>
+<h2 class="price">$${menu.price}</h2>
+</div>
+`
+      )
+    );
+}
+
+DOM.nonvegan.addEventListener("click", function () {
+  DOM.menupage.innerHTML = "";
+  nonvegan();
+});
+
+function nonvegan() {
+  menu
     .filter((menu) => menu.vegan === "false")
+    .forEach((menu) =>
+      DOM.menupage.insertAdjacentHTML(
+        "beforeend",
+        `
+  <div class="child">
+<img class="img" src="${menu.img}"><img>
+<h1 class="price">${menu.name}</h1>
+<h2 class="price">$${menu.price}</h2>
+</div>
+`
+      )
+    );
+}
+
+DOM.halal.addEventListener("click", function () {
+  DOM.menupage.innerHTML = "";
+  halal();
+});
+
+function halal() {
+  menu
+    .filter((menu) => menu.halal === "true")
+    .forEach((menu) =>
+      DOM.menupage.insertAdjacentHTML(
+        "beforeend",
+        `
+  <div class="child">
+<img class="img" src="${menu.img}"><img>
+<h1 class="price">${menu.name}</h1>
+<h2 class="price">$${menu.price}</h2>
+</div>
+`
+      )
+    );
+}
+DOM.nonhalal.addEventListener("click", function () {
+  DOM.menupage.innerHTML = "";
+  nonhalal();
+});
+function nonhalal() {
+  menu
+    .filter((menu) => menu.halal === "false")
     .forEach((menu) =>
       DOM.menupage.insertAdjacentHTML(
         "beforeend",
