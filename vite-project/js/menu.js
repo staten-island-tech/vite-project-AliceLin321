e42699c7430adb4168de065a7e78377653cbac5a
AliceLin321 <ataechiml24@gmail.com>
2023-01-04 01:44:06 -0500

    Alice Lin Bon Vite APp

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -29,7 +29,7 @@ const menu = [
     halal: "false",
   },
   {
-    name: "ham bahn mi",
+    name: "Ham BahnMi",
     vegan: "false",
     price: 7,
     img: "https://www.happyfoodstube.com/wp-content/uploads/2018/08/vietnamese-sandwich-banh-mi-image.jpg",
@@ -37,7 +37,7 @@ const menu = [
   },
 
   {
-    name: "grilled pork bahn mi",
+    name: "Pork BahnMi",
     vegan: "false",
     price: 7,
     img: "https://rasamalaysia.com/wp-content/uploads/2012/06/lemongrass-pork-banhmi-thum-1.jpg",
@@ -45,7 +45,7 @@ const menu = [
   },
 
   {
-    name: "curry chicken w/ bread",
+    name: "Curry Chicken",
     vegan: "false",
     price: 10,
     img: "https://www.wandercooks.com/wp-content/uploads/2022/01/vietnamese-chicken-curry-5-683x1024.jpg",
@@ -53,7 +53,7 @@ const menu = [
   },
 
   {
-    name: "bun cha",
+    name: "Bun Cha",
     vegan: "false",
     price: 9,
     img: "https://image.essen-und-trinken.de/11950450/t/Ax/v10/w1440/r1.7778/-/fleisch-nudel-bowl-bun-cha-jpg--64061-.jpg",
@@ -61,7 +61,7 @@ const menu = [
   },
 
   {
-    name: "bahn xeo crepe",
+    name: "Bahn Xeo",
     vegan: "false",
     price: 7,
     img: "https://takestwoeggs.com/wp-content/uploads/2022/04/Ba%CC%81nh-Xe%CC%80o-Vietnamese-Cre%CC%82pes-Takestwoeggs-Final-Photogrphy-sq.jpg",
@@ -69,7 +69,7 @@ const menu = [
   },
 
   {
-    name: "Veggie Bun bo Hue",
+    name: "Veggie Bun BoHue",
     vegan: "true",
     price: 11,
     img: "https://woonheng.com/wp-content/uploads/2020/12/Bun-Bo-Hue-Chay-1-scaled.jpg",
@@ -85,7 +85,7 @@ const menu = [
   },
 
   {
-    name: "cha ca",
+    name: "Cha Ca",
     vegan: "false",
     price: 9,
     img: "https://www.cooking-therapy.com/wp-content/uploads/2020/06/Vietnamese-Turmeric-Fish-scaled.jpg",
@@ -93,7 +93,7 @@ const menu = [
   },
 
   {
-    name: "ga nuong",
+    name: "Ga Nuong",
     vegan: "false",
     price: 12,
     img: "https://www.theforkbite.com/wp-content/uploads/2019/11/Bun-Ga-Nuong-featured-1200x1200-1.jpg",
@@ -101,7 +101,7 @@ const menu = [
   },
 
   {
-    name: "Nom hoa chuoi",
+    name: "Nom Hoa Chuoi",
     vegan: "true",
     price: 6,
     img: "https://www.vietnamonline.com/media/uploads/froala_editor/images/VNO_NomHoaChuoi.jpg",
@@ -109,7 +109,7 @@ const menu = [
   },
 
   {
-    name: "bahn knot",
+    name: "Bahn Knot",
     vegan: "false",
     price: 3,
     img: "https://www.wokandkin.com/wp-content/uploads/2021/02/Banh-Khot-Close-Up-saved-for-web.png",
